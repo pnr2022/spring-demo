ef18e17973a4e9e2ecb21228cbc8d3f9d81c5e6f
pnreddy2016 <pnreddy2016@gmail.com>
2022-02-12 17:00:54 -0600

    Updated code 02/12 at 5pm

--- src/main/java/com/springdemo/example/SpringDemoApplication.java
+++ src/main/java/com/springdemo/example/SpringDemoApplication.java
@@ -2,13 +2,25 @@ package com.springdemo.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringDemoApplication {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!!!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
-		System.out.println("AAAAAAA");
+		
 	}
 
+	
+	
+	
+	
 }
